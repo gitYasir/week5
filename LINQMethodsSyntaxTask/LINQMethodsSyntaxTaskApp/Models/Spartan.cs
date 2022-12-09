787f9897ac8f4108b397d6c6aacfadbc9cb34744
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-12-09 11:33:00 +0000

    task started

---
+++ LINQMethodsSyntaxTask/LINQMethodsSyntaxTaskApp/Models/Spartan.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace LINQMethodsSyntaxTaskApp.Models;
+
+public partial class Spartan
+{
+    public int Id { get; set; }
+
+    public string Title { get; set; } = null!;
+
+    public string Firstname { get; set; } = null!;
+
+    public string Lastname { get; set; } = null!;
+
+    public string? UniverityAttended { get; set; }
+
+    public string? CourseTaken { get; set; }
+
+    public string? MarkAchieved { get; set; }
+
+    public string City { get; set; } = null!;
+
+    public string Country { get; set; } = null!;
+}
---
+++ LINQMethodsSyntaxTask/LINQMethodsSyntaxTaskApp/Program.cs
@@ -0,0 +1,7 @@
+﻿namespace LINQMethodsSyntaxTaskApp {
+    internal class Program {
+        static void Main( string[] args ) {
+            Console.WriteLine( "Hello, World!" );
+        }
+    }
+}
