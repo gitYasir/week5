209d296698a26255803b3927719d880f6ca290e5
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-12-11 16:01:56 +0000

    1.5 southwind task done

---
+++ EF_ModelFirst_Starter/EF_ModelFirst/CustomerManager.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF_ModelFirst {
+    public class CustomerManager {
+        public CustomerManager() {
+        }
+
+        public static void CreateCustomer( Customer cust ) {
+            using ( var db = new SouthwindContext() ) {
+                // create customer
+                var query = db.Customers.Add( cust );
+                db.SaveChanges();
+            }
+        }
+
+        public static void ReadCustomers() {
+            using ( var db = new SouthwindContext() ) {
+                //read current customers table records
+                foreach ( var cus in db.Customers ) {
+                    Console.WriteLine( $"Customer ID: {cus.CustomerId} Name: {cus.ContactName} City: {cus.City} Postcode: {cus.PostalCode} Country: {cus.Country}" );
+                }
+            }
+        }
+
+        public static void UpdateCustomer( Customer updatedCust ) {
+            using ( var db = new SouthwindContext() ) {
+                // update customer
+                var query = db.Customers
+                 .Where( w => w.CustomerId == updatedCust.CustomerId );
+
+                foreach ( var cust in query ) {
+                    if ( cust.ContactName != updatedCust.ContactName ) {
+                        cust.ContactName = updatedCust.ContactName;
+                    }
+                    else if ( cust.City != updatedCust.City ) {
+                        cust.City = updatedCust.City;
+                    }
+                    else if ( cust.PostalCode != updatedCust.PostalCode ) {
+                        cust.PostalCode = updatedCust.PostalCode;
+                    }
+                    else if ( cust.Country != updatedCust.Country ) {
+                        cust.Country = updatedCust.Country;
+                    }
+                }
+
+                db.SaveChanges();
+            }
+        }
+
+        public static void DeleteCustomer( string id ) {
+            using ( var db = new SouthwindContext() ) {
+                // delete customer
+                var query = db.Customers.Where( w => w.CustomerId == id ).ExecuteDelete();
+                db.SaveChanges();
+            }
+        }
+    }
+
+}
--- EF_ModelFirst_Starter/EF_ModelFirst/Program.cs
+++ EF_ModelFirst_Starter/EF_ModelFirst/Program.cs
@@ -6,35 +6,19 @@ using System.Linq;
 namespace EF_ModelFirst {
     class Program {
         static void Main( string[] args ) {
-            using ( var db = new SouthwindContext() ) {
-                // create customer
-                //Customer cust = new() {
-                //    CustomerId = "5",
-                //    ContactName = "Steve",
-                //    City = "Luton",
-                //    PostalCode = "XXXX-XXX"
-                //};
-                //var query = db.Customers.Add( cust );
 
-                // delete customer
-                //var query = db.Customers
-                //    .Where( w => w.CustomerId == "1" ).ExecuteDelete();
 
-                //update customer
-                //var query = db.Customers
-                //    .Where( w => w.CustomerId == "3" );
+            Customer custstomer = new() {
+                CustomerId = "2",
+                ContactName = "Ibra",
+                City = "Abuja",
+                PostalCode = "XXXX-XXX",
+                Country = "Nigeria"
+            };
 
-                //foreach ( var cust in query ) {
-                //    cust.City = "Brighton";
-                //}
+            CustomerManager.UpdateCustomer( custstomer );
 
-                //db.SaveChanges();
-                //read current customers table records
-                //foreach ( var cust in db.Customers ) {
-                //    Console.WriteLine( $"Customer ID: {cust.CustomerId} Name: {cust.ContactName} City: {cust.City} Postcode: {cust.PostalCode}" );
-                //}
 
-            }
         }
     }
 }
