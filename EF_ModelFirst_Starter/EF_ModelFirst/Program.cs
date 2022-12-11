60b9324554b7ddad7e0b8bc5bb9f3d39562351eb
Yasir <yasir.ibrahim@hotmail.co.uk>
2022-12-11 13:50:14 +0000

    1.1 of southwind exercises done

--- EF_ModelFirst_Starter/EF_ModelFirst/Program.cs
+++ EF_ModelFirst_Starter/EF_ModelFirst/Program.cs
@@ -7,14 +7,33 @@ namespace EF_ModelFirst {
     class Program {
         static void Main( string[] args ) {
             using ( var db = new SouthwindContext() ) {
-                Customer cust = new() {
-                    CustomerId = "5",
-                    ContactName = "Steve",
-                    City = "Luton",
-                    PostalCode = "XXXX-XXX"
-                };
-                var query = db.Customers.Add( cust );
+                // create customer
+                //Customer cust = new() {
+                //    CustomerId = "5",
+                //    ContactName = "Steve",
+                //    City = "Luton",
+                //    PostalCode = "XXXX-XXX"
+                //};
+                //var query = db.Customers.Add( cust );
+
+                // delete customer
+                //var query = db.Customers
+                //    .Where( w => w.CustomerId == "1" ).ExecuteDelete();
+
+                //update customer
+                //var query = db.Customers
+                //    .Where( w => w.CustomerId == "3" );
+
+                //foreach ( var cust in query ) {
+                //    cust.City = "Brighton";
+                //}
+
                 db.SaveChanges();
+                // read current customers table records
+                foreach ( var cust in db.Customers ) {
+                    Console.WriteLine( $"Customer ID: {cust.CustomerId} Name: {cust.ContactName} City: {cust.City} Postcode: {cust.PostalCode}" );
+                }
+
             }
         }
     }
